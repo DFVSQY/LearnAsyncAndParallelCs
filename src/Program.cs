4339b9fc93a90aa5b824a354ac383b3342cc1939
DFVSQY <dfvsqy@qq.com>
2023-07-01 00:06:40 +0800

    lock的基础使用

--- src/Program.cs
+++ src/Program.cs
@@ -4,10 +4,31 @@ namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
+        static bool done;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Code");
+            Thread thread = new Thread(Run);
+            thread.Start();
+
+            Run();
+        }
+
+        private static object _locker = new object();
+        static void Run()
+        {
+            lock (_locker)
+            {
+                if (!done)
+                {
+                    Console.WriteLine("run!");
+                    done = true;
+                }
+                else
+                {
+                    Console.WriteLine("no run!");
+                }
+            }
         }
     }
 }

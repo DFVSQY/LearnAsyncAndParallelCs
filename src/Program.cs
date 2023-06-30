cac32689fac0f99d65b0d6fb231bb6f48c5822ab
DFVSQY <dfvsqy@qq.com>
2023-07-01 01:10:08 +0800

    一个简单的信号发送实例

--- src/Program.cs
+++ src/Program.cs
@@ -4,15 +4,23 @@ namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
+        private static ManualResetEvent signal = new ManualResetEvent(false);
+
         static void Main(string[] args)
         {
-            Thread thread = new Thread(() => { Print("hello world"); });
+            Thread thread = new Thread(Run);
             thread.Start();
+
+            Thread.Sleep(2000);
+            signal.Set();
         }
 
-        static void Print(string msg)
+        static void Run()
         {
-            Console.WriteLine(msg);
+            Console.WriteLine("start wait one signal");
+            signal.WaitOne();
+            signal.Dispose();
+            Console.WriteLine("finish one signal");
         }
     }
 }

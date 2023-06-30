694145684f4b7f6abfc9aba7adc37d6931f5ee0b
DFVSQY <dfvsqy@qq.com>
2023-07-01 00:14:51 +0800

    向Thread传递参数的一种方式

--- src/Program.cs
+++ src/Program.cs
@@ -4,31 +4,15 @@ namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        static bool done;
-
         static void Main(string[] args)
         {
-            Thread thread = new Thread(Run);
+            Thread thread = new Thread(() => { Print("hello world"); });
             thread.Start();
-
-            Run();
         }
 
-        private static object _locker = new object();
-        static void Run()
+        static void Print(string msg)
         {
-            lock (_locker)
-            {
-                if (!done)
-                {
-                    Console.WriteLine("run!");
-                    done = true;
-                }
-                else
-                {
-                    Console.WriteLine("no run!");
-                }
-            }
+            Console.WriteLine(msg);
         }
     }
 }

4242ee24aaab2a4b242f674f83f2032ad5b16d5f
DFVSQY <dfvsqy@qq.com>
2023-07-01 18:45:47 +0800

    使用TaskCompletionSource的一个简单示例

--- src/Program.cs
+++ src/Program.cs
@@ -6,41 +6,19 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Task<int> task = Task.Run(Run);
+            TaskCompletionSource<int> tcs = new TaskCompletionSource<int>();
 
-            /*
-            使用ConfigureAwait (false)来告诉指明不需要恢复到原来的同步上下文，
-            而是可以在任何可用的线程上继续执行后续的代码。
-            */
-            var waiter = task.ConfigureAwait(false).GetAwaiter();
-
-            /* 不一定在主线程执行 */
-            waiter.OnCompleted(() =>
+            new Thread(() =>
             {
-                /*
-                如果先导任务出现错误，则当延续代码调用awaiter.GetResult()的时候将会重新抛出异常。
-                当然我们也可以访问先导任务的Result属性而不是调用GetResult方法。
-                但如果先导任务失败，则调用GetResult方法就可以直接得到原始的异常，而不是包装后的AggregateException。
-                因此，这种方式可以实现更加简洁清晰的catch代码块。
-                
-                对于非泛型任务，GetResult的返回值为void，而这个函数的用途完全是为了重新抛出异常。
-                */
-                int result = waiter.GetResult();
-                Console.WriteLine("result:" + result.ToString() + " processorID:" + Thread.GetCurrentProcessorId().ToString());
-            });
+                Thread.Sleep(5000);
+                tcs.SetResult(42);
+            })
+            {
+                IsBackground = true,
+            }.Start();
 
-            Console.WriteLine("iscompleted:" + task.IsCompleted + " processorID:" + Thread.GetCurrentProcessorId().ToString());
-
-            Console.ReadLine();
-        }
-
-        static int Run()
-        {
-            Console.WriteLine("start run task, processorID:" + Thread.GetCurrentProcessorId().ToString());
-            int sum = 0;
-            for (int i = 1; i <= 100; i++) sum += i;
-            Console.WriteLine("finish run task");
-            return sum;
+            Task<int> task = tcs.Task;
+            Console.WriteLine(task.Result);
         }
     }
 }

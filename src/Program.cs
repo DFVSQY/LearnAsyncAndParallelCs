1ed9343b8c1363a16f82b110038a85a64ca3c5df
DFVSQY <dfvsqy@qq.com>
2023-07-01 19:05:28 +0800

    Task静态方法Delay使用的一个简单示例

--- src/Program.cs
+++ src/Program.cs
@@ -6,26 +6,13 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Delay(5000).GetAwaiter().OnCompleted(() =>
+            Task.Delay(5000).GetAwaiter().OnCompleted(() =>
             {
                 Console.WriteLine("task finish");
             });
 
             Console.WriteLine("waiting task ...");
             Console.ReadLine();
-        }
-
-        /*
-        TaskCompletionSource的真正作用是创建一个不绑定线程的任务。
-        可以利用TaskCompletionSource实现一个非堵塞的Delay方法
-        */
-        static Task Delay(int milliseconds)
-        {
-            var tcs = new TaskCompletionSource<object>();
-            var timer = new System.Timers.Timer(milliseconds) { AutoReset = false };
-            timer.Elapsed += delegate { timer.Dispose(); tcs.SetResult(null); };
-            timer.Start();
-            return tcs.Task;
         }
     }
 }

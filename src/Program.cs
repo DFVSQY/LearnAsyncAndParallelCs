de9138c06a72e56911b818163e9f6bed2289f211
DFVSQY <dfvsqy@qq.com>
2023-07-02 20:18:45 +0800

    Async和Await关键字的基础使用示例

--- src/Program.cs
+++ src/Program.cs
@@ -4,15 +4,35 @@ namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Task.Delay(5000).GetAwaiter().OnCompleted(() =>
-            {
-                Console.WriteLine("task finish");
-            });
+            Console.WriteLine("main thread processorID:{0}", Thread.GetCurrentProcessorId());
 
-            Console.WriteLine("waiting task ...");
-            Console.ReadLine();
+            /*
+            以下两句代码等效于：
+            var awaiter = GetPrimesCountAsync(2, 1000000).GetAwaiter();
+            awaiter.OnCompleted(() =>
+            {
+                int result = awaiter.GetResult();
+                Console.WriteLine("result:{0} processorID:{1}", result, Thread.GetCurrentProcessorId());
+            });
+            */
+            int result = await GetPrimesCountAsync(2, 1000000);
+
+            // 以下的代码不一定在主线程执行了
+            Console.WriteLine("result:{0} processorID:{1}", result, Thread.GetCurrentProcessorId());
+        }
+
+        static Task<int> GetPrimesCountAsync(int start, int count)
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine("start runing ...");
+                int cnt = ParallelEnumerable.Range(start, count).Count(n =>
+                Enumerable.Range(2, (int)Math.Sqrt(n) - 1).All(i => n % i > 0));
+                Console.WriteLine("finish running, processorID:{0}", Thread.GetCurrentProcessorId());
+                return cnt;
+            });
         }
     }
 }

6b2787c437c7445d9d571ea47c682af817f6373d
DFVSQY <dfvsqy@qq.com>
2023-07-04 01:03:38 +0800

    使用WebClient的DownloadDataTaskAsync方法测试Async和Await

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -8,40 +9,42 @@ namespace MyApp // Note: actual namespace depends on the project name.
         {
             Console.WriteLine("main thread processorID:{0}", Thread.GetCurrentProcessorId());
 
-            DisplayPrimeCounts();
+            Go();
 
             Console.WriteLine("waiting task, processorID:{0}", Thread.GetCurrentProcessorId());
 
             Console.ReadLine();
         }
 
-        /*
-        在第一次执行GetPrimesCountAsync方法时，由于出现了await表达式，因此执行点返回给调用者。
-        当方法完成（或者出错）时，执行点会从停止之处恢复执行，同时保留本地变量和循环计数器的值。
-        */
-        static async void DisplayPrimeCounts()
+        static async void Go()
         {
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine("run idx:{0} processorID:{1} time:{2}", i, Thread.GetCurrentProcessorId(), DateTime.Now.Ticks / 1000);
+            string[] urls = new string[]{
+                "https://www.baidu.com/",
+                "https://www.sohu.com/",
+                "https://www.sina.com.cn/",
+            };
 
-                int result = await GetPrimesCountAsync(i * 1000000 + 2, 1000000);
-                Console.WriteLine("result:{0} processorID:{1}", result, Thread.GetCurrentProcessorId());
+            try
+            {
+                Console.WriteLine("start go, processorID:{0}", Thread.GetCurrentProcessorId());
+                foreach (string url in urls)
+                {
+                    WebClient webClient = new WebClient();
+                    byte[] data = await webClient.DownloadDataTaskAsync(url);
+                    Console.WriteLine("url:{0} len:{1} processorID:{2}", url, data.Length, Thread.GetCurrentProcessorId());
+                }
+                Console.WriteLine("finish go, processorID:{0}", Thread.GetCurrentProcessorId());
             }
-
-            Console.WriteLine("skip DisplayPrimeCounts, processorID:{0}", Thread.GetCurrentProcessorId());
-        }
-
-        static Task<int> GetPrimesCountAsync(int start, int count)
-        {
-            return Task.Run(() =>
+            catch (WebException e)
             {
-                Console.WriteLine("start runinng, processorID:{0}", Thread.GetCurrentProcessorId());
-                int cnt = ParallelEnumerable.Range(start, count).Count(n =>
-                Enumerable.Range(2, (int)Math.Sqrt(n) - 1).All(i => n % i > 0));
-                Console.WriteLine("finish running, processorID:{0}", Thread.GetCurrentProcessorId());
-                return cnt;
-            });
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("finally, processorID:{0}", Thread.GetCurrentProcessorId());
+            }
         }
+
+
     }
 }

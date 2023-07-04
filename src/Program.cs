1cf9770ed8a4ee0b304df5abcc8b315f16d95a04
DFVSQY <dfvsqy@qq.com>
2023-07-05 00:11:44 +0800

    异步函数的编写之返回Task<TResult>

--- src/Program.cs
+++ src/Program.cs
@@ -19,8 +19,8 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static async void Run()
         {
             Console.WriteLine("start run, processorID:{0}", Thread.GetCurrentProcessorId());
-            await PrintAnswer();
-            Console.WriteLine("finish run, processorID:{0}", Thread.GetCurrentProcessorId());
+            int answer = await PrintAnswer();
+            Console.WriteLine("finish run, answer:{0}, processorID:{1}", answer, Thread.GetCurrentProcessorId());
         }
 
         /// <summary>
@@ -29,21 +29,23 @@ namespace MyApp // Note: actual namespace depends on the project name.
         /// 这样就很容易创建异步调用链。
         /// 编译器会展开异步函数，使用TaskCompletionSource创建一个任务，并将Task返回。
         /// 其展开版本类似于PrintAnswerExpand函数。
+        /// 异步函数中若方法体返回TResult则函数的返回值为Task<TResult>
         /// </summary>
-        static async Task PrintAnswer()
+        static async Task<int> PrintAnswer()
         {
             Console.WriteLine("start printanswer, processorID:{0}", Thread.GetCurrentProcessorId());
             await Task.Delay(5000);
             int answer = 5 * 12;
             Console.WriteLine("answer:{0}, processorID:{1}", answer, Thread.GetCurrentProcessorId());
+            return answer;
         }
 
         /// <summary>
         /// PrintAnswer的编译器展开版本
         /// </summary>
-        static Task PrintAnswerExpand()
+        static Task<int> PrintAnswerExpand()
         {
-            var tcs = new TaskCompletionSource<object>();
+            var tcs = new TaskCompletionSource<int>();
             var awaiter = Task.Delay(5000).GetAwaiter();
             awaiter.OnCompleted(() =>
             {
@@ -52,7 +54,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
                     awaiter.GetResult();     // Re-throw any exceptions
                     int answer = 5 * 12;
                     Console.WriteLine("answer:{0}, processorID:{1}", answer, Thread.GetCurrentProcessorId());
-                    tcs.SetResult(null);
+                    tcs.SetResult(answer);
                 }
                 catch (Exception ex) { tcs.SetException(ex); }
             });

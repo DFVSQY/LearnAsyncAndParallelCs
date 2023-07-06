9f70370a784023b596562445931100b83869ead7
DFVSQY <dfvsqy@qq.com>
2023-07-07 00:29:42 +0800

    一个Progress<T>和IProgress<T>的使用示例

--- src/Program.cs
+++ src/Program.cs
@@ -9,32 +9,40 @@ namespace MyApp // Note: actual namespace depends on the project name.
         {
             Console.WriteLine("main thread processorID:{0}", Thread.GetCurrentProcessorId());
 
-            // CancellationToken cancellationToken = new CancellationToken();
+            /*
+            Progress<T>类的构造器可以接受一个Action<T>委托并对其进行包装。
+            (Progress<T>还有一个ProgressChanged事件。我们可以订阅这个事件而无须在构造函数中传入一个委托)
+            在Progress<int>实例化时，如果拥有同步上下文，该类就会捕获同步上下文。
+            当Run调用Report方法时，该对象就会使用同步上下文调用委托。
+            */
+            Progress<int> progress = new Progress<int>((value) =>
+            {
+                Console.WriteLine("prg:{0}% processorID:{1}", value, Thread.GetCurrentProcessorId());
+            });
 
-            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(6000);
-            await Run(cancellationTokenSource.Token);
+            await Run(progress);
 
-            Console.WriteLine("all finish, processorID:{0}", Thread.GetCurrentProcessorId());
+            Console.WriteLine("processorID:{0}", Thread.GetCurrentProcessorId());
+
+            Console.ReadLine();
         }
 
-        static async Task Run(CancellationToken cancellation)
+        static Task Run(IProgress<int> progress)
         {
-            try
+            return Task.Run(() =>
             {
-                for (int i = 0; i < 5; i++)
+                Console.WriteLine("start run, processorID:{0}", Thread.GetCurrentProcessorId());
+                for (int i = 1; i <= 1000; i++)
                 {
-                    Console.WriteLine("i:{0} processorID:{1}", i, Thread.GetCurrentProcessorId());
-                    await Task.Delay(5000, cancellation);
+                    if (i % 10 == 0)
+                    {
+                        int prg = i / 10;
+                        Console.WriteLine("value:{0} processorID:{1}", prg, Thread.GetCurrentProcessorId());
+                        progress.Report(prg);
+                    }
                 }
-            }
-            catch (OperationCanceledException e)
-            {
-                Console.WriteLine("msg:{0} processorID:{1}", e.Message, Thread.GetCurrentProcessorId());
-            }
-            finally
-            {
-                Console.WriteLine("run finally, processorID:{0}", Thread.GetCurrentProcessorId());
-            }
+                Console.WriteLine("finish run, processorID:{0}", Thread.GetCurrentProcessorId());
+            });
         }
     }
 }

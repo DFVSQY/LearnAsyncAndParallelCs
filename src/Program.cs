3ca161817a995a7577194da32cbf81decd9621ee
DFVSQY <dfvsqy@qq.com>
2023-07-08 00:31:12 +0800

    任务组合器WhenAll的简单使用示例

--- src/Program.cs
+++ src/Program.cs
@@ -9,10 +9,20 @@ namespace MyApp // Note: actual namespace depends on the project name.
         {
             Console.WriteLine("start, now:{0} processorID:{1}", DateTime.Now, Thread.GetCurrentProcessorId());
 
-            Task<int> task = await Task.WhenAny(Delay1(), Delay2(), Delay3());
-            int result = await task;
-
-            Console.WriteLine("finish, result:{0} now:{1} processorID:{2}", result, DateTime.Now, Thread.GetCurrentProcessorId());
+            /*
+            Task.WhenAll只在所有的任务完成之后才会完成，即使中间出现了错误也一样。
+            如果多个任务发生了错误，那么这些异常会组合到任务的AggregateException中
+            （这也是AggregateException真正发挥作用的时候，你可以从中得到所有的异常）。
+            */
+            try
+            {
+                int[] results = await Task.WhenAll(Delay1(), Delay2(), Delay3());
+                Console.WriteLine("finish, result's num:{0} now:{1} processorID:{2}", results.Length, DateTime.Now, Thread.GetCurrentProcessorId());
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine("exception num:{0}", e.InnerExceptions.Count);
+            }
         }
 
         static async Task<int> Delay1()

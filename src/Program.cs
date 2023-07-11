c13e80f7086c9f388b7f6bfb63d3fe5e2a06293a
DFVSQY <dfvsqy@qq.com>
2023-07-12 00:09:40 +0800

    排它锁之Monitor.Enter/Monitor.Exit的基本使用

--- src/Program.cs
+++ src/Program.cs
@@ -28,11 +28,20 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Go()
         {
             /*
-            每一次只能有一个线程锁定同步对象（本例中的locker），而其他线程则被阻塞，直至锁释放。
-            如果参与竞争的线程多于一个，则它们需要在准备队列中排队，并以先到先得的方式获得锁。
-            排它锁会强制以所谓序列（serialized）的方式访问被锁保护的资源，因为线程之间的访问是不能重叠的。
+            事实上，C#的lock语句是包裹在try/finally语句块中的Monitor.Enter和Monitor.Exit语法糖。
+
+            如果在Monitor.Enter和try语句块之间抛出了异常（例如，调用该线程的Abort方法，或者抛出了OutOfMemoryException）那么锁的状态是不确定的。
+            但若已经获得了锁，那么这个锁就永远无法释放，因为我们已经没有机会进入try/finally代码块了。因此这种情况会造成锁泄露。
+
+            为了防范这种风险，CLR 4在设计时对Monitor.Enter进行了重载，增加了lockTaken参数。
+
+            Enter方法执行结束后，当且仅当该方法执行时抛出了异常且没有获得锁时，lockTaken为false。
+            因而从C# 4.0开始，lock语句将会翻译为以下模式（该模式较之前更加健壮）。
             */
-            lock (locker)
+
+            bool lockTaken = false;
+            Monitor.Enter(locker, ref lockTaken);
+            try
             {
                 if (var2 != 0)
                 {
@@ -44,6 +53,13 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 }
 
                 var2 = 0;
+            }
+            finally
+            {
+                if (lockTaken)
+                {
+                    Monitor.Exit(locker);
+                }
             }
         }
     }

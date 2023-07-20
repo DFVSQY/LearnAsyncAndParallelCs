5445f3139a7b8f46ab623c253d7433d2d21addf0
DFVSQY <dfvsqy@qq.com>
2023-07-20 22:20:03 +0800

    跨进程同步之Mutex使用示例

--- src/Program.cs
+++ src/Program.cs
@@ -5,38 +5,39 @@ namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("start, now:{0} processorID:{1}", DateTime.Now, Thread.GetCurrentProcessorId());
+            /*
+            Mutex和C#的lock类似，但是它可以支持多个进程。获得或者释放Mutex比lock要慢。
 
-            object locker1 = new object();
-            object locker2 = new object();
-
-            new Thread(() =>
+            Mutex类的WaitOne方法将获得该锁，ReleaseMutex方法将释放该锁。Mutex只能在获得锁的线程释放锁。
+            */
+            using (var mutex = new Mutex(true, "example.com OneAtATimeDemo"))
             {
-                lock (locker1)
+                /*
+                Mutex.WaitOne(TimeSpan timeout, bool exitContext)函数是一个实例方法，
+                它可以封锁当前线程，直到当前的Mutex对象收到信号或超时为止，同时指定是否在等待之前退出同步域。
+                */
+                if (!mutex.WaitOne(TimeSpan.FromSeconds(3), false))
                 {
-                    Console.WriteLine("start new thread");
-                    Thread.Sleep(1000);
-                    lock (locker2)
-                    {                      // Deadlock
-                        Console.WriteLine("new thread!");
-                    }
+                    Console.WriteLine("Another instance of the app is running. Bye! ");
+                    return;
                 }
-            }).Start();
-
-            lock (locker2)
-            {
-                Console.WriteLine("start main thread");
-                Thread.Sleep(1000);
-                lock (locker1)              // Deadlock
+                try
                 {
-                    Console.WriteLine("main thread");
+                    RunProgram();
+                }
+                finally
+                {
+                    mutex.ReleaseMutex();
                 }
             }
-
-            Console.WriteLine("finish!");
         }
 
+        static void RunProgram()
+        {
+            Console.WriteLine("Running. Press Enter to exit");
+            Console.ReadLine();
+        }
     }
 }

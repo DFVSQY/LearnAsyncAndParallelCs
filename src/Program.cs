80445982681016d440f5dd10b10ea5d3c843a738
DFVSQY <dfvsqy@qq.com>
2023-07-22 13:34:25 +0800

    等待句柄之ManualResetEvent的使用示例

--- src/Program.cs
+++ src/Program.cs
@@ -7,46 +7,39 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main()
         {
-            new Thread(Run).Start();
+            for (int i = 1; i <= 5; i++)
+            {
+                int id = i;
+                new Thread(Run).Start(id);
+            }
 
-            waitReady.WaitOne();
-            lock (locker) msg = "001";
-            waitGo.Set();
+            Thread.Sleep(1000);
 
-            waitReady.WaitOne();
-            lock (locker) msg = "002";
-            waitGo.Set();
-
-            waitReady.WaitOne();
-            lock (locker) msg = null;
-            waitGo.Set();
+            waitEvent.Set();
         }
 
         /*
-        假设主线程需要向工作线程连续发送三次信号。
-        如果主线程单纯地连续调用Set方法若干次，那么第二次或者第三次发送的信号就有可能丢失，因为工作线程需要时间来处理每一次的信号。
-        其解决方案是主线程等待工作线程准备就绪之后再发送信号。这可以通过引入另一个AutoResetEvent来实现。
+        ManualResetEvent的作用就像是一个大门。
+        调用Set方法就开启大门，并允许任意数目的调用WaitOne方法的线程通过大门。
+        而调用Reset方法则会关闭大门。在大门关闭时调用WaitOne方法会发生阻塞。
+        而当大门再次打开时，线程会立刻释放。除这些区别之外，ManualResetEvent的功能和AutoResetEvent是一样的。
+
+        .NET 4.0引入了一种新的ManualResetEvent称为ManualResetEventSlim。
+        后者对短时期的等待进行了优化，即选择进行几个迭代的自旋操作。
+        此外，它还拥有更加高效的托管实现。
+        并支持在Wait时使用Can-cellationToken取消等待操作。但是它不能进行跨进程的信号发送。
+        ManualResetEventSlim并没有从WaitHandle派生。
+        但是它拥有一个WaitHandle属性，访问该属性将返回一个（使用传统等待句柄性能配置的）WaitHandle派生类型的对象。
+
+        ManualResetEvent适用于用一个线程来释放其他所有线程的情形，而CountdownEvent则适用于相反的情形。
         */
-        static AutoResetEvent waitReady = new AutoResetEvent(false);
-        static AutoResetEvent waitGo = new AutoResetEvent(false);
+        static ManualResetEvent waitEvent = new ManualResetEvent(false);
 
-        static object locker = new object();
-
-        static string? msg;
-
-        static void Run()
+        static void Run(object id)
         {
-            while (true)
-            {
-                waitReady.Set();
-                waitGo.WaitOne();
-
-                lock (locker)
-                {
-                    if (msg == null) return;
-                    Console.WriteLine(msg);
-                }
-            }
+            Console.WriteLine("{0} waiting ...", id);
+            waitEvent.WaitOne();
+            Console.WriteLine("{0} notified", id);
         }
     }
 }

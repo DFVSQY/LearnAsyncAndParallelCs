3ec15d83d43d82ebdb40a7d20d16e08a9e7c1900
DFVSQY <dfvsqy@qq.com>
2023-07-22 17:01:46 +0800

    延迟初始化Lazy<T>的线程安全版本的使用示例

--- src/Program.cs
+++ src/Program.cs
@@ -6,27 +6,32 @@ namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        /*
-        Barrier类实现了一个线程执行屏障（thread execution barrier）。
-        它允许多个线程在同一时刻汇合。这个类的执行速度很快，非常高效。它是基于Wait、Pulse和自旋锁实现的。
-
-        使用这个类的步骤如下：
-            1. 创建Barrier实例。指定参与汇合的线程的数量（此后还可以调用AddPartici-pants/RemoveParticipants方法对这个数量进行更改）。
-            2. 当需要汇合时，在每一个线程上都调用SignalAndWait。
-
-        创建Barrier对象时还可以指定一个后续操作（post-phase），这个功能非常有用。
-        该操作是一个委托，它会在SignalAndWait调用n次之后，所有线程释放之前执行
-
-        后续操作适用于从各个工作线程获得数据。它不需要担心抢占的问题，因为在它执行过程中所有的工作线程都会被阻塞。
-        */
-        static Barrier barrier = new Barrier(3, (br) =>
+        class A
         {
-            Console.WriteLine();
-        });
+            public A()
+            {
+                Console.WriteLine("A ctor call");
+            }
+        }
+
+        /*
+        从.NET Framework 4.0开始引入了Lazy<T>类，该类实现了延迟初始化（lazy initialization）功能。
+        如果实例化时以true为参数，则它就会使用线程安全的初始化模式。
+        
+        Lazy<T>实际上还在锁上进行了微小的优化，称为“双检锁”（doublech-ecked lock）。
+        双检锁执行一次volatile读操作，避免在对象初始化后进行锁操作。
+        
+        使用Lazy<T>时可以传入一个工厂委托方法来指明如何创建一个新的实例，同时传入第二个参数true，
+        之后就可以使用Value属性访问实例的值了
+        */
+        static Lazy<A> lazy_a = new Lazy<A>(() =>
+        {
+            return new A();
+        }, true);
 
         static void Main(string[] args)
         {
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 10; i++)
             {
                 new Thread(Run).Start();
             }
@@ -34,11 +39,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
         private static void Run()
         {
-            for (int i = 0; i < 5; i++)
-            {
-                Console.Write("{0} ", i);
-                barrier.SignalAndWait();
-            }
+            A a = lazy_a.Value;
         }
     }
 }

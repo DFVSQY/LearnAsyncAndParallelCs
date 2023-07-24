f4d28569681ada34248b2b89a513912640613d3a
DFVSQY <dfvsqy@qq.com>
2023-07-25 00:54:33 +0800

    PLINQ之使用AsParallel进行数据并行查询的简单示例

--- src/Program.cs
+++ src/Program.cs
@@ -9,37 +9,31 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("start main thread:{0}", Thread.CurrentThread.ManagedThreadId);
+            IEnumerable<int> numbers = Enumerable.Range(3, 100000 - 3);
 
             /*
-            .NET Framework在System.Timers命名空间中提供了另外一个同名定时器类。
-            它简单包装了System.Threading.Timer，在相同底层引擎的基础上提供了额外的易用性。
+            PLINQ可以自动并行化本地LINQ查询。易于使用是PLINQ的优势，因为它将工作划分和结果整理的任务交给了Framework。
+            要使用PLINQ只需直接在输入序列上调用AsParallel()方法，而后和先前一样编写普通的LINQ查询即可。
 
-            它的附加功能：
-                1. 实现了IComponent接口，允许嵌入到Visual Studio设计器的组件托盘中。
-                2. 提供了Interval属性替代Change方法。
-                3. 提供了Elapsed事件取代回调委托。
-                4. 提供了Enabled属性来开始和停止计时器（默认值为false）。
-                5. 如果不习惯使用Enabled属性还可以使用Start和Stop方法。
-                6. 提供了AutoReset标志，用于指示重复的事件（默认值为true）。
-                7. 提供了SynchronizingObject属性。可调用该对象的Invoke和BeginInvoke方法安全地调用WPF元素和Windows Forms控件的方法。
+            AsParallel是System.Linq.ParallelEnumerable类的一个扩展方法，它将输入包装为一个以ParallelQuery<TSource>为基类的序列，
+            这样，后续的LINQ查询运算符就会绑定到由ParallelEnumerable定义的另外一套扩展方法上。
+            这些扩展方法为每一种标准查询运算符提供了并行化实现。
+            基本上，它们的工作原理都是将输入序列划分为小块，并将每一块在不同的线程上执行，并将执行结果整理为一个输出序列以供使用。
+
+            调用AsSequential()会将ParallelQuery序列包装解除，后续的查询运算符将会重新绑定到标准查询运算符上并顺序执行。
+            这在调用有副作用或者非线程安全的代码之前是非常必要的。
             */
-            using (System.Timers.Timer timer = new System.Timers.Timer())
+            var parallelQuery =
+              from n in numbers.AsParallel()
+              where Enumerable.Range(2, (int)Math.Sqrt(n)).All(i => n % i > 0)
+              select n;
+
+            int[] primes = parallelQuery.ToArray();
+
+            foreach (int prime in primes)
             {
-                timer.Interval = 1000;
-                timer.Elapsed += Run;
-
-                timer.Start();
-                Console.ReadLine();
-                timer.Stop();
+                Console.Write("{0} ", prime);
             }
-
-            Console.WriteLine("finish main thread:{0}", Thread.CurrentThread.ManagedThreadId);
-        }
-
-        private static void Run(object? sender, EventArgs e)
-        {
-            Console.WriteLine("tick ..., thread:{0}", Thread.CurrentThread.ManagedThreadId);
         }
     }
 }

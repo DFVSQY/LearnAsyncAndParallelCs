999a18738b0e0880041d7f549650b71f22f7b973
DFVSQY <dfvsqy@qq.com>
2023-07-29 04:10:07 +0800

    创建Task之指定TaskCreationOptions选项

--- src/Program.cs
+++ src/Program.cs
@@ -11,26 +11,16 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main()
         {
             /*
-            Task.Run方法会创建并启动一个Task或者Task<TResult>对象。
-            这个方法实际上和Task.Factory.StartNew是等价的。而后者具有更多的重载版本，也更具灵活性。
-            */
+            在调用StartNew方法（或在实例化Task对象）时，可以指定一个TaskCreationOptions枚举值来调整任务的执行方式。
+            TaskCreationOptions是一个标志枚举类型。
 
-            /*
-            Task.Factory.StartNew方法可以指定一个状态对象，这个对象会作为参数传递给目标方法，因此目标方法的签名中也必须包含一个object类型的参数。
-            该方式可以避免在Lambda表达式中直接调用Run而造成闭包开销。这种优化并不明显，因此实践中很少使用。
+            LongRunning字段会通知调度器为任务指定一个线程。这种方式非常适合I/O密集型任务和长时间执行的任务。
+            如果不这样做，那么那些执行时间很短的任务反而可能需要等待很长的时间才能被调度。
             */
-            Task task1 = Task.Factory.StartNew(Run, "vscode");
-            task1.Wait();
+            Task task = Task.Factory.StartNew(Run, "Hello Task", TaskCreationOptions.LongRunning);
+            task.Wait();
 
-            Console.WriteLine("task1 finish");
-
-            /*
-            可以利用状态对象来给任务指定一个有意义的名称。之后就可以使用AsyncState属性查询这个名称。
-            Visual Studio在并行任务窗口中会显示每一个任务的AsyncState，因此取一个有意义的名字可以让调试变得更加轻松。
-            */
-            Task task2 = Task.Factory.StartNew(state => { Run("vscode"); }, "stateInfo");
-            task2.Wait();
-            Console.WriteLine("task2 finish, state:{0}", task2.AsyncState);
+            Console.WriteLine("all finish");
         }
 
         static void Run(object? msg)

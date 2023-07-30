7fcc45328ba6dd3587f0dbc0ec41723339250b39
DFVSQY <dfvsqy@qq.com>
2023-07-30 23:12:53 +0800

    Task之延续任务的TaskContinuationOptions的使用示例

--- src/Program.cs
+++ src/Program.cs
@@ -19,22 +19,29 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
             重新抛出前导任务中的异常是一种安全的处理方式。
             只要有程序调用延续任务的Wait方法，则该异常就会继续传播，并在Wait方法中重新抛出。
-            */
 
+            另一种处理异常的方式是为异常和正常的结果指定不同的延续任务。
+            指定TaskContinuationOptions就可以做到这一点。
+            */
             Task task = Task.Factory.StartNew(() =>
             {
                 throw null;
-            }).ContinueWith((task1) =>
-            {
-                task1.Wait();
-            }).ContinueWith((task2) =>
-            {
-                task2.Wait();
             });
+
+            Task error = task.ContinueWith((t1) =>
+            {
+                Console.WriteLine("exp msg:{0}", t1.Exception?.Message);
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
+            Task ok = task.ContinueWith((t1) =>
+            {
+                Console.WriteLine("task finish");
+            }, TaskContinuationOptions.NotOnFaulted);
 
             try
             {
-                task.Wait();
+                ok.Wait();
+                error.Wait();
             }
             catch (Exception ex)
             {
@@ -46,6 +53,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
                 if (e != null)
                 {
+                    // 任务取消异常，System.Threading.Tasks.TaskCanceledException
                     Console.WriteLine("inner ex:{0}", e.GetType());
                 }
             }

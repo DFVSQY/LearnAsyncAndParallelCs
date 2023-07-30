8722446e3dd718dc8bca9642a690dbd3d2111d05
DFVSQY <dfvsqy@qq.com>
2023-07-30 23:44:04 +0800

    Task之延续任务与子任务结合使用示例

--- src/Program.cs
+++ src/Program.cs
@@ -10,53 +10,49 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main()
         {
-            /*
-            延续任务可以查询前导任务的Exception属性来确认前导任务是否已经失败。
-            当然，也可以调用Result/Wait并捕获AggregateException。
-            如果前导任务失败，则延续任务既不确认，也不获得前导任务的结果，则前导任务的异常就成为未观测异常。
-            之后，当垃圾回收器回收前导任务时就会触发TaskScheduler.Unobserved-TaskException事件。
-            重新抛出前导任务中的异常是一种安全的处理方式。只要有程序调用延续任务的Wait方法，则该异常就会继续传播，并在Wait方法中重新抛出。
+            TaskCreationOptions options = TaskCreationOptions.AttachedToParent;
 
-            重新抛出前导任务中的异常是一种安全的处理方式。
-            只要有程序调用延续任务的Wait方法，则该异常就会继续传播，并在Wait方法中重新抛出。
-
-            另一种处理异常的方式是为异常和正常的结果指定不同的延续任务。
-            指定TaskContinuationOptions就可以做到这一点。
-            */
             Task task = Task.Factory.StartNew(() =>
             {
-                throw null;
+                Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine("child task1");
+                    Thread.Sleep(1000);
+                    throw null;
+                }, options);
+
+                Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine("child task2");
+                    Thread.Sleep(3000);
+                    throw null;
+                }, options);
+
+                Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine("child task3");
+                    Thread.Sleep(5000);
+                    throw null;
+                }, options);
             });
 
-            Task error = task.ContinueWith((t1) =>
+            /*
+            延续任务有一个非常重要的特性：它在所有的子任务完成之后才会开始执行。
+            这时，子任务抛出的所有异常都会封送到延续任务中。
+            */
+            Task error = task.ContinueWith((t) =>
             {
-                Console.WriteLine("exp msg:{0}", t1.Exception?.Message);
+                if (t.Exception is AggregateException)
+                {
+                    AggregateException aggregate = t.Exception as AggregateException;
+                    foreach (Exception ex in aggregate.InnerExceptions)
+                    {
+                        Console.WriteLine("type:{0} msg:{1}", ex.InnerException?.GetType(), ex.InnerException?.Message);
+                    }
+                }
             }, TaskContinuationOptions.OnlyOnFaulted);
 
-            Task ok = task.ContinueWith((t1) =>
-            {
-                Console.WriteLine("task finish");
-            }, TaskContinuationOptions.NotOnFaulted);
-
-            try
-            {
-                ok.Wait();
-                error.Wait();
-            }
-            catch (Exception ex)
-            {
-                Exception? e = ex.InnerException;
-                while (e != null && e is AggregateException)
-                {
-                    e = e.InnerException;
-                }
-
-                if (e != null)
-                {
-                    // 任务取消异常，System.Threading.Tasks.TaskCanceledException
-                    Console.WriteLine("inner ex:{0}", e.GetType());
-                }
-            }
+            error.Wait();
 
             Console.WriteLine("all finish");
         }

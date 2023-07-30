8646ce59786530b874de878e1fa89c2571820c81
DFVSQY <dfvsqy@qq.com>
2023-07-30 21:11:01 +0800

    Task之延续任务的异常的传播和重抛示例

--- src/Program.cs
+++ src/Program.cs
@@ -11,22 +11,44 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main()
         {
             /*
-            延续任务和普通任务一样，其类型也可以是Task<TResult>类型并返回数据。
-            在下面的示例中，我们将使用一串任务来计算Math.Sqrt(8 * 2)，并输出结果。
+            延续任务可以查询前导任务的Exception属性来确认前导任务是否已经失败。
+            当然，也可以调用Result/Wait并捕获AggregateException。
+            如果前导任务失败，则延续任务既不确认，也不获得前导任务的结果，则前导任务的异常就成为未观测异常。
+            之后，当垃圾回收器回收前导任务时就会触发TaskScheduler.Unobserved-TaskException事件。
+            重新抛出前导任务中的异常是一种安全的处理方式。只要有程序调用延续任务的Wait方法，则该异常就会继续传播，并在Wait方法中重新抛出。
+
+            重新抛出前导任务中的异常是一种安全的处理方式。
+            只要有程序调用延续任务的Wait方法，则该异常就会继续传播，并在Wait方法中重新抛出。
             */
-            Task<double> task = Task.Factory.StartNew(() =>
+
+            Task task = Task.Factory.StartNew(() =>
             {
-                return 8;
+                throw null;
             }).ContinueWith((task1) =>
             {
-                return task1.Result * 2;
+                task1.Wait();
             }).ContinueWith((task2) =>
             {
-                return Math.Sqrt(task2.Result);
+                task2.Wait();
             });
 
-            task.Wait();
-            Console.WriteLine("Result:{0}", task.Result);
+            try
+            {
+                task.Wait();
+            }
+            catch (Exception ex)
+            {
+                Exception? e = ex.InnerException;
+                while (e != null && e is AggregateException)
+                {
+                    e = e.InnerException;
+                }
+
+                if (e != null)
+                {
+                    Console.WriteLine("inner ex:{0}", e.GetType());
+                }
+            }
 
             Console.WriteLine("all finish");
         }
